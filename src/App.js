3ab591cccec4c8a163d7f38940868cae2ed5797c
Gurinder Bhangu <gurinderbhangu@hotmail.com>
2021-02-09 13:06:43 -0600

    overhaul all displays

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import About from './About';
-import './App.css';
-import Contact from './Contact';
-import Education from './Education';
-import Projects from './Projects';
-import WorkExperience from './WorkExperience';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Testimonials from "./Components/Testimonials";
+import Portfolio from "./Components/Portfolio";
+
+import "./App.css";
+
+const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
+
   return (
-    <div className="app">
-      <About />
-      <Education />
-      <WorkExperience />
-      <Projects />
-      <Contact />
+    <div className="App">
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Resume data={resumeData.resume} />
+      <Portfolio data={resumeData.portfolio} />
+      {/* <Testimonials data={resumeData.testimonials} /> */}
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
     </div>
   );
-}
+};
 
 export default App;
